78fd8371cbf22a19921557b128ccc476a0f61a0b
Laurian-Dufrechou <lauriandugers@hotmail.fr>
2023-04-06 23:53:48 +0200

    docs(Makefile, interf, use): Makefile, interf, use

---
+++ src/parser_laurian/use.ml
@@ -0,0 +1,16 @@
+
+#load "lang.cmo";;
+#load "parser.cmo" ;;
+#load "lexer.cmo" ;;
+#load "interf.cmo";;
+
+open Interf;;
+open Lang;;
+
+(* For using the parser:
+
+- Evaluate this file (use.ml)
+- parse "Tests/progsimple.c" ;;
+
+*)
+
