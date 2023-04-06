add79589f0c567a2203402135015381dc7faad1d
Laurian-Dufrechou <lauriandugers@hotmail.fr>
2023-04-06 22:59:20 +0200

    feat(Parser and Lexer): Added Parser.mly and Lexer.mll

---
+++ src/parser_laurian/Tests/progsimple.c
@@ -0,0 +1,12 @@
+void main (string y, int x) {
+    x = 15;
+    while (x < 20) {
+        x = x + 1;
+        y = "hello";
+        if (x == 17) {
+            y = "world";
+        }
+        /*  */
+    }
+    return x;
+}
